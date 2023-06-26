f09538ecfe47d21148ada39f8dedb0ba5ec7278a
Mus Maxim <musmaxim.musienko@yandex.ru>
2023-06-26 23:07:42 +0300

    исправление замечаний ревьюера

--- cypress/e2e/BurgerConstructor.cy.js
+++ cypress/e2e/BurgerConstructor.cy.js
@@ -3,7 +3,7 @@ import { BURGER_API } from "../../src/utils/data";
 describe("тестирование BurgerConstructor", () => {
   beforeEach(() => {
     cy.viewport(1400, 800);
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
     cy.intercept("GET", `${BURGER_API}ingredients`, {
       fixture: "ingredients.json",
     });
@@ -15,49 +15,64 @@ describe("тестирование BurgerConstructor", () => {
 
   it("тестирование модалок", () => {
     cy.wait(1000);
-    cy.get("[data-testid=dragElement]").eq(0).click();
-    cy.get("[data-testid=modal]").should("be.visible");
+    cy.get(testIds.dragElement).eq(0).click();
+    cy.get(testIds.modal).should("be.visible");
     cy.url().should("include", "/ingredients/60d3b41abdacab0026a733c6");
-    cy.get("[data-testid=modalTitle]").should(
+    cy.get(testIds.modalTitle).should(
       "have.text",
       "Детали ингредиента"
     );
-    cy.get("[data-testid=ingredientImage]").should("be.visible");
-    cy.get("[data-testid=ingredientName]").should("not.be.empty");
-    cy.get("[data-testid=ingredientInfo]").should("not.be.empty");
-    cy.get("[data-testid=btnCloseModal]").click();
-    cy.get("[data-testid=modal]").should("not.exist");
+    cy.get(testIds.ingredientImage).should("be.visible");
+    cy.get(testIds.ingredientName).should("not.be.empty");
+    cy.get(testIds.ingredientInfo).should("not.be.empty");
+    cy.get(testIds.btnCloseModal).click();
+    cy.get(testIds.modal).should("not.exist");
   });
 
   it("тестирование логина, dnd, оформление заказа", () => {
 
-    cy.visit("http://localhost:3000/react-burger#/login");
+    cy.visit("login");
     cy.wait(1000);
-    cy.get("[data-testid=testInputEmail]").type("test@test.ru");
-    cy.get("[data-testid=testInputPassword]").type("test");
-    cy.get("[data-testid=testBtnLogin]").click();
+    cy.get(testIds.testInputEmail).type("test@test.ru");
+    cy.get(testIds.testInputPassword).type("test");
+    cy.get(testIds.testBtnLogin).click();
 
-    cy.get("[data-testid=dragElement]").should("exist");
-    cy.get("[data-testid=dragElement]").eq(0).trigger("dragstart");
-    cy.get("[data-testid=constructorContainer]").should("exist");
-    cy.get("[data-testid=constructorContainer]").trigger("drop");
+    cy.get(testIds.dragElement).should("exist");
+    cy.get(testIds.dragElement).eq(0).trigger("dragstart");
+    cy.get(testIds.constructorContainer).should("exist");
+    cy.get(testIds.constructorContainer).trigger("drop");
 
-    cy.get("[data-testid=dragElement]").eq(3).trigger("dragstart");
-    cy.get("[data-testid=constructorContainer]").trigger("drop");
+    cy.get(testIds.dragElement).eq(3).trigger("dragstart");
+    cy.get(testIds.constructorContainer).trigger("drop");
 
-    cy.get("[data-testid=dragElement]").eq(6).trigger("dragstart");
-    cy.get("[data-testid=constructorContainer]").trigger("drop");
+    cy.get(testIds.dragElement).eq(6).trigger("dragstart");
+    cy.get(testIds.constructorContainer).trigger("drop");
 
-    cy.get("[data-testid=dragElement]").eq(10).trigger("dragstart");
-    cy.get("[data-testid=constructorContainer]").trigger("drop");
+    cy.get(testIds.dragElement).eq(10).trigger("dragstart");
+    cy.get(testIds.constructorContainer).trigger("drop");
 
     cy.get(`[data-testid=btnMakeOrder]`)
       .should("exist")
       .and("contain", "Оформить заказ")
       .click();
-    cy.get("[data-testid=modal]").should("be.visible");
-    cy.get("[data-testid=orderNumber]").should("not.be.empty");
-    cy.get("[data-testid=btnCloseModal]").click();
-    cy.get("[data-testid=modal]").should("not.exist");
+    cy.get(testIds.modal).should("be.visible");
+    cy.get(testIds.orderNumber).should("not.be.empty");
+    cy.get(testIds.btnCloseModal).click();
+    cy.get(testIds.modal).should("not.exist");
   });
 });
+
+const testIds = {
+  dragElement: "[data-testid=dragElement]",
+  modal: "[data-testid=modal]",
+  modalTitle: "[data-testid=modalTitle]",
+  ingredientImage: "[data-testid=ingredientImage]",
+  ingredientName: "[data-testid=ingredientName]",
+  ingredientInfo: "[data-testid=ingredientInfo]",
+  btnCloseModal: "[data-testid=btnCloseModal]",
+  testInputEmail: "[data-testid=testInputEmail]",
+  testInputPassword: "[data-testid=testInputPassword]",
+  testBtnLogin: "[data-testid=testBtnLogin]",
+  constructorContainer: "[data-testid=constructorContainer]",
+  orderNumber: "[data-testid=orderNumber]",
+};
